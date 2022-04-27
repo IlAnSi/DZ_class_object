626804d8c98edaa06c9571477293cfc47ccaa94d
IlAnSi <sia.muromez@gmail.com>
2022-04-27 20:12:07 +0600

    DZ object and class #2

--- OOP_KVIZ.py
+++ OOP_KVIZ.py
@@ -10,11 +10,32 @@ class Student:
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
+    def rate_hw(self, lectur, course, grade):
+        if isinstance(lectur, Lecturer) and course in self.courses_in_progress and course in lectur.courses_attached:
+            if course in lectur.grades:
+                lectur.grades[course] += [grade]
+            else:
+                lectur.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+        self.courses_attached = []
+
+
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        self.courses_in_progress = []
         self.courses_attached = []
 
     def rate_hw(self, student, course, grade):
@@ -27,22 +48,11 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        self.courses_attached = []
-
-
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        self.courses_attached = []
-
-
-
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
@@ -50,4 +60,3 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
-print(Lecturer.mro())
